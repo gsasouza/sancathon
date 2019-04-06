05a4e526ae104b166839a0af95b78291871a48c0
Gabriel Souza <gabrie.ssouza@gmail.com>
2019-04-06 14:53:33 -0300

    add user crud

---
+++ backend/src/graphql/QueryType.js
@@ -0,0 +1,39 @@
+import { GraphQLObjectType, GraphQLString, GraphQLNonNull, GraphQLID } from 'graphql';
+import { connectionArgs, fromGlobalId } from 'graphql-relay';
+
+import UserType from '../modules/user/UserType';
+import UserConnection from '../modules/user/UserConnection';
+import * as UserLoader from '../modules/user/UserLoader';
+
+export default new GraphQLObjectType({
+  name: 'Query',
+  description: 'The root of all queries',
+  fields: () => ({
+    me: {
+      type: UserType,
+      resolve: (root, args, context) => (context.user ? UserLoader.load(context, context.user._id) : null),
+    },
+    user: {
+      type: UserType,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLID),
+        },
+      },
+      resolve: (obj, args, context) => {
+        const { id } = fromGlobalId(args.id);
+        return UserLoader.load(context, id);
+      },
+    },
+    users: {
+      type: UserConnection.connectionType,
+      args: {
+        ...connectionArgs,
+        search: {
+          type: GraphQLString,
+        },
+      },
+      resolve: (obj, args, context) => UserLoader.loadUsers(context, args),
+    },
+  }),
+});
