5431dcb53d4e493393dc25d48ce8ba2e7effe1af
Joao Vitor Silva Ramos <joaoramosj@usp.br>
2019-04-06 19:01:58 -0300

    add Login screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 import Layout from './modules/Layout';
 import { SnackbarProvider } from './modules/snackbar';
-import OwnerProductList from './modules/product/OwnerProductList';
+import Login from './modules/user/Login.jsx';
 
 const theme = createMuiTheme({
   palette: {
@@ -23,7 +23,7 @@ class App extends Component {
       <MuiThemeProvider theme={theme}>
         <SnackbarProvider>
           <Layout>
-            <OwnerProductList/>
+            <Login/>
           </Layout>
         </SnackbarProvider>
       </MuiThemeProvider>
--- frontend/src/modules/user/Login.jsx
+++ frontend/src/modules/user/Login.jsx
@@ -1,26 +1,41 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import { withFormik } from 'formik';
-import * as Yup from 'Yup';
+import TextField from '@material-ui/core/TextField';
+
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content : center;
+  height : 100%
 `;
 
 const LoginInnerForm = () => {
   return (
     <Wrapper>
+      <TextField
+            required
+            id="outlined-required"
+            label="Usuario"
+            defaultValue="Hello World"
+            margin="normal"
+            variant="outlined"
+      />
 
+      <TextField
+            required
+            id="outlined-required"
+            label="Senha"
+            defaultValue="Hello World"
+            margin="normal"
+            variant="outlined"
+      />
     </Wrapper>
   )
 };
 
 export default withFormik({
   mapPropsToValues: () => ({ username: '', password: '' }),
-  validationSchema: Yup.object().shape({
-    name: Yup.string().required(),
-    password: Yup.string().required(),
-  }),
-  handleSubmit: () => console.log('here');
+  handleSubmit: () => console.log('here')
 })(LoginInnerForm);
